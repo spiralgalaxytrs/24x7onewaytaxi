67da0dc55cca4c68d7c89527b13ed34e64883c7f
Kumar-manoj-T <tmanojkumar022003@gmail.com>
2025-07-03 17:22:46 +0530

    Update distance display in calcRoute function to show accurate values

--- js/main.js
+++ js/main.js
@@ -60,13 +60,13 @@ function calcRoute() {
 
           //  $("#output").html("<div class='result-table'> Driving distance: " + result.routes[0].legs[0].distance.text + ".<br />SUV Rate: ₹" +((n)*12)+"-"+((n)*15)+".<br />Sedan Rate: ₹" +((n)*8)+"-"+((n)*11)+".<br />Hatchback Rate: ₹" +((n)*16)+"-"+((n)*19)+ ".<br />Duration: " + result.routes[0].legs[0].duration.text + ".</div>");
             //document.getElementById("output").style.display = "block";
-            $("#output1").html("<div class='meta-item;'>  <span> Distance: " + km + "</span><br /><span> One Way : ₹" +sedano+"</span><br /><span> Round Way : ₹" +sedanr+"</span></div>");
+            $("#output1").html("<div class='meta-item;'>  <span> Distance: " + n + "</span><br /><span> One Way : ₹" +sedano+"</span><br /><span> Round Way : ₹" +sedanr+"</span></div>");
             document.getElementById("output1").style.display = "block";
-            $("#output2").html("<div class='meta-item;'>  <span> Distance: " + km + "</span><br /><span> One Way : ₹" +suvo+"</span><br /><span> Round Way : ₹" +suvr+"</span></div>");
+            $("#output2").html("<div class='meta-item;'>  <span> Distance: " + n + "</span><br /><span> One Way : ₹" +suvo+"</span><br /><span> Round Way : ₹" +suvr+"</span></div>");
             document.getElementById("output1").style.display = "block";
-            $("#output3").html("<div class='meta-item;'>  <span> Distance: " + km + "</span><br /><span> Round Way : ₹" +traveller+"</span><br /><span> One Way : Not available </span></div>");
+            $("#output3").html("<div class='meta-item;'>  <span> Distance: " + n + "</span><br /><span> Round Way : ₹" +traveller+"</span><br /><span> One Way : Not available </span></div>");
             document.getElementById("output1").style.display = "block";
-             $("#output4").html("<div class='meta-item;'>  <span> Distance: " + km + "</span><br /><span> One Way : ₹" +crystao+"</span><br /><span> Round Way : ₹" +crystar+"</span></div>");
+             $("#output4").html("<div class='meta-item;'>  <span> Distance: " + n + "</span><br /><span> One Way : ₹" +crystao+"</span><br /><span> Round Way : ₹" +crystar+"</span></div>");
             document.getElementById("output1").style.display = "block";
             //display route <span> Price  :  ₹13 / Km.</span>
             directionsDisplay.setDirections(result);
